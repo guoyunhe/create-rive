8eb506192134becd406905cfccf55ef367ddd558
Guo Yunhe <i@guoyunhe.me>
2024-02-03 14:15:53 +0800

    fix: project location

--- src/create-rive.ts
+++ src/create-rive.ts
@@ -8,9 +8,7 @@ import { fileURLToPath } from 'url';
 import { init } from './init/init.js';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const packageJson = JSON.parse(
-  readFileSync(join(__dirname, '../package.json'), 'utf-8'),
-);
+const packageJson = JSON.parse(readFileSync(join(__dirname, '../package.json'), 'utf-8'));
 
 i18n.configure({
   locales: ['en', 'zh'],
@@ -25,19 +23,10 @@ program
   .argument('[project]', i18n.__('project_argument_desc'))
   .option('-t, --template <name>', i18n.__('template_option_desc'))
   .option('-e, --esm-only', i18n.__('esm_only_option_desc'))
-  .option(
-    '-p, --package-manager <name>',
-    i18n.__('package_manager_option_desc'),
-    'npm',
-  )
   .action(init);
 
 program.helpOption('-h, --help', i18n.__('help_option_desc'));
 
-program.version(
-  packageJson.version,
-  '-v, --version',
-  i18n.__('version_option_desc'),
-);
+program.version(packageJson.version, '-v, --version', i18n.__('version_option_desc'));
 
 program.parse();
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,76 @@
-export * from './init/init.js';
+import chalk from 'chalk';
+import fs from 'fs-extra';
+import { init } from 'init-roll';
+import { basename, dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { runCommand } from './init/runCommand';
+
+export interface InitOptions {
+  /**
+   * Template to use.
+   *
+   * @default "react"
+   */
+  template?: 'react' | 'react-icons' | 'node' | 'cli';
+  /**
+   * By default, rive generates both CJS and ESM. If esmOnly is enabled, CJS will NOT be generated.
+   */
+  esmOnly: boolean;
+}
+
+export async function createRive(project: string | null, { template, esmOnly }: InitOptions) {
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+
+  const root = project ? join(process.cwd(), project) : process.cwd();
+
+  let packageJson: any;
+  try {
+    packageJson = await fs.readJSON(join(root, 'package.json'), {
+      throws: false,
+    });
+  } catch (e) {
+    //
+  }
+
+  const name = packageJson?.name || basename(root);
+  const _template = template || packageJson?.rive?.template || 'react';
+  const params = {
+    name,
+    description: packageJson.description || '',
+    esmOnly,
+    basename: basename(name),
+  };
+
+  console.log('Selected template:', chalk.cyan(_template));
+
+  console.log('Generating files...');
+  await init(join(__dirname, '../template/base'), root, params);
+  switch (_template) {
+    case 'node':
+      await init(join(__dirname, '../template/node'), root, params);
+      break;
+    case 'cli':
+      await init(join(__dirname, '../template/node'), root, params);
+      await init(join(__dirname, '../template/cli'), root, params);
+      break;
+    case 'react':
+      await init(join(__dirname, '../template/react'), root, params);
+      break;
+    case 'react-icons':
+      await init(join(__dirname, '../template/react'), root, params);
+      await init(join(__dirname, '../template/react-icons'), root, params);
+      break;
+    default:
+      break;
+  }
+
+  console.log(chalk.green('Done'));
+
+  console.log('Installing node modules...');
+  await runCommand(`${process.env.npm_command} update`);
+  console.log(chalk.green('Done'));
+
+  console.log('Formating source codes...');
+  await runCommand(`${process.env.npm_command} run lint:fix`);
+  console.log(chalk.green('Done'));
+}
